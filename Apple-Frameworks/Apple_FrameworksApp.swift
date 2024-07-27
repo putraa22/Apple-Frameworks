be15c3de7aa95294ca8b13b03088f9215fdbff76
Hermansyah Putra <hermansyahputra0418@gmail.com>
2024-07-27 17:39:43 +0700

    Initial Commit

---
+++ Apple-Frameworks/Apple_FrameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  Apple_FrameworksApp.swift
+//  Apple-Frameworks
+//
+//  Created by Putra on 27/07/24.
+//
+
+import SwiftUI
+
+@main
+struct Apple_FrameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
